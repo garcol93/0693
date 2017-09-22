f88a1329fb0d369b1ed732800012d0d59bafe46d
garcol93 <omargc1993@gmail.com>
2017-09-22 12:51:36 +0200

    muestra nombre + edad + lugar nacimiento

--- nombre.java
+++ nombre.java
@@ -1,6 +1,7 @@
 public class nombre{
 	public static void main(String args[]){
- 	System.out.println("Omar Garcia");
-       System.out.println("24 años");
+ 	 System.out.println("Omar Garcia");
+         System.out.println("24 años");
+	 System.out.println("Nacido en: Valencia");
  }
 }
